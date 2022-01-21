2a4ab2a4bc23305fda8483ea8b59eeb4bf82bdbe
unknown <bastien.rig@gmail.com>
2022-01-21 15:57:32 +0100

    add acvd processor

--- Processors/Processors.py
+++ Processors/Processors.py
@@ -6,6 +6,19 @@
 # bastien.rigaud@univ-rennes1.fr
 # Description:
 
+import numpy as np
+import vtk
+import pyvista as pv
+import pyacvd
+
+def _check_keys_(input_features, keys):
+    if type(keys) is list or type(keys) is tuple:
+        for key in keys:
+            assert key in input_features.keys(), 'Make sure the key you are referring to is present in the features, ' \
+                                                 '{} was not found'.format(key)
+    else:
+        assert keys in input_features.keys(), 'Make sure the key you are referring to is present in the features, ' \
+                                              '{} was not found'.format(keys)
 
 class Processor(object):
     def pre_process(self, input_features):
@@ -15,7 +28,24 @@ class Processor(object):
         return input_features
 
 class ACVD_resampling(Processor):
-    def __init__(self):
+    def __init__(self, input_keys=('input_name',), output_keys=('output_name',), np_points=(5000,)):
+        self.input_keys = input_keys
+        self.output_keys = output_keys
+        self.np_points = np_points
+
+    def compute_acvd(self, polydata, nb_points):
+        pv_temp = pv.PolyData(polydata)
+        cluster = pyacvd.Clustering(pv_temp)
+        cluster.cluster(int(nb_points))
+        remesh = cluster.create_mesh()
+        remesh_vtk = vtk.vtkPolyData()
+        remesh_vtk.SetPoints(remesh.GetPoints())
+        remesh_vtk.SetVerts(remesh.GetVerts())
+        remesh_vtk.SetPolys(remesh.GetPolys())
+        return remesh_vtk
 
     def pre_process(self, input_features):
-        
+        _check_keys_(input_features)
+        for input_key, output_key, np_point in zip(self.input_keys,self.output_keys, self.np_points):
+            input_features[output_key] = self.compute_acvd(input_features[input_key], np_point)
+
--- README.md
+++ README.md
@@ -34,3 +34,6 @@ This need framework relies on ArrayFire 3.6.2 (cpu or CUDA backend) https://arra
 ## References
 - Rigaud, B., Cazoulat, G., Vedam, S., Venkatesan, A. M., Peterson, C. B., Taku, N., ... & Brock, K. K. (2020). Modeling complex deformations of the sigmoid colon between external beam radiation therapy and brachytherapy images of cervical cancer. International Journal of Radiation Oncology* Biology* Physics, 106(5), 1084-1094.
 - Rigaud, B., Simon, A., Gobeli, M., Leseur, J., Duvergé, L., Williaume, D., ... & De Crevoisier, R. (2018). Statistical shape model to generate a planning library for cervical adaptive radiotherapy. IEEE transactions on medical imaging, 38(2), 406-416.
+
+## Inspiration
+[Brian Mark Anderson](https://github.com/brianmanderson)
